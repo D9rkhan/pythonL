e82746a2c1b24458049025e79e2f53974b6711fa
clay <claymoreisfellasleep@gmail.com>
2023-09-04 02:20:10 +0600

    нужно посмотреть numexpr

---
+++ library.py
@@ -0,0 +1,20 @@
+# numexpr библиотека чтобы вывести итог математической операции
+import numexpr
+
+expr = input("Введите математическую операцию: ")
+
+result = numexpr.eva
+
+#colorama библиотека чтобы перекрасить текст в консоли
+from colorama import init
+from colorama import Fore, Back, Style
+# запуск библиотеки
+init()
+
+#Fore цвет текста
+print(Fore.CYAN)
+
+#Back цвет фона
+print(Back.BLACK)
+
+print("Hello colored text!")
