e25beaf2acdaa2ad4baae46f56bbff77a46041b9
Darkhan <D9rkhan@gmail.com>
2023-09-04 17:53:05 +0600

    Посмотрю дома bot

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, filters, ConversationHandler
 
 # Задайте состояния (если вам нужны)
 STATE_1 = 1
@@ -19,16 +19,16 @@ def unknown(update, context):
 
 def main():
     # Замените 'YOUR_BOT_TOKEN' на ваш токен, который вы получили от @BotFather
-    updater = Updater(token='6492517339:AAHdrxJ0qyoSDbgrfBVoxlyPKN3zlZUTNsg', use_context=True)
+    updater = Updater(token='6682782561:AAGYIRKhal8qyfXYlaF2rzmskoQKAtrYBS0', use_context=True)
 
     dp = updater.dispatcher
 
     # Добавьте обработчики команды /start и текстовых сообщений
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dp.add_handler(MessageHandler(filters.text & ~filters.command, echo))
 
     # Добавьте обработчик неизвестных команд
-    dp.add_handler(MessageHandler(Filters.command, unknown))
+    dp.add_handler(MessageHandler(filters.command, unknown))
 
     # Запустите бота
     updater.start_polling()
