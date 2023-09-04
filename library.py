b31fab9c7a0a87cec7dbb9bd4ed15d00ebe523e5
Darkhan <D9rkhan@gmail.com>
2023-09-04 09:40:51 +0600

    Библиотеки colorama, numexpr и сбор программы в exe

--- library.py
+++ library.py
@@ -1,15 +1,15 @@
 # numexpr библиотека чтобы вывести итог математической операции
 import numexpr
 
-expr = input("Введите математическую операцию: ")
-
-result = numexpr.eva
-
 #colorama библиотека чтобы перекрасить текст в консоли
 from colorama import init
 from colorama import Fore, Back, Style
 # запуск библиотеки
 init()
+
+expr = input("Введите математическую операцию: ")
+
+result = numexpr.evaluate(expr)
 
 #Fore цвет текста
 print(Fore.CYAN)
@@ -17,4 +17,11 @@ print(Fore.CYAN)
 #Back цвет фона
 print(Back.BLACK)
 
-print("Hello colored text!")
+print(f"Результат: {result}")
+
+input()
+
+"""
+Можно собрать программу .exe с помощью библиотеки pyinstaller
+Вводим команду pyinstaller (Название файла с расширением) -F
+"""
